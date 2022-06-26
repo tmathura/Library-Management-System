9de8fe86f18b8466f4c902532321118499a1b813
tmathura <tmathura@gmail.com>
2022-06-27 00:43:22 +0200

    Add starter fun things.

--- StarterTreats/StarterTreats.SimpleTasks.UnitTests/SimpleTasksTestMemberData.cs
+++ StarterTreats/StarterTreats.SimpleTasks.UnitTests/SimpleTasksTestMemberData.cs
@@ -15,5 +15,9 @@
             yield return new object[] { 250, false };
             yield return new object[] { 256, true };
         }
+        public static IEnumerable<object[]> ReturnOddNumbersBetween1And100Data()
+        {
+            yield return new object[] { new List<int> { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99 } };
+        }
     }
 }
--- StarterTreats/StarterTreats.SimpleTasks.UnitTests/SimpleTasksTests.cs
+++ StarterTreats/StarterTreats.SimpleTasks.UnitTests/SimpleTasksTests.cs
@@ -1,7 +1,16 @@
+using Xunit.Abstractions;
+
 namespace StarterTreats.SimpleTasks.UnitTests
 {
     public class SimpleTasksTests
     {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public SimpleTasksTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
         [Theory]
         [MemberData(nameof(SimpleTasksTestMemberData.NumberIsPowerOfTwoData), MemberType = typeof(SimpleTasksTestMemberData))]
         public void NumberIsPowerOfTwo(int number, bool expected)
@@ -11,6 +20,73 @@ namespace StarterTreats.SimpleTasks.UnitTests
 
             // Assert
             Assert.Equal(expected, isPowerOfTwo);
+        }
+
+        [Theory]
+        [InlineData("Hello", "olleH")]
+        [InlineData("ReverseString", "gnirtSesreveR")]
+        [InlineData("DeVeLoPeR", "RePoLeVeD")]
+        [InlineData("glue", "eulg")]
+        [InlineData("Copy Me", "eM ypoC")]
+        public void ReverseString(string stringValue, string expected)
+        {
+            // Act
+            var result = SimpleTasks.ReverseString(stringValue);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("", "The supplied value is null")]
+        [InlineData(null, "The supplied value is null")]
+        public void ReverseString_OnEmptyOrNull(string stringValue, string expected)
+        {
+            // Act
+            var exception = Assert.Throws<Exception>(() => SimpleTasks.ReverseString(stringValue));
+
+            // Assert
+            Assert.Equal(expected, exception.Message);
+        }
+
+        [Theory]
+        [InlineData("Hi", "HiHiHi", 3)]
+        [InlineData("Hello", "HelloHelloHelloHelloHello", 5)]
+        [InlineData("Bye", "ByeByeByeByeByeByeByeByeBye", 9)]
+        public void RepeatString(string stringValue, string expected, int numberOfTimesToRepeat)
+        {
+            // Act
+            var result = SimpleTasks.RepeatString(stringValue, numberOfTimesToRepeat);
+
+            // Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Theory]
+        [InlineData("", "The supplied value is null", 10)]
+        [InlineData(null, "The supplied value is null", 11)]
+        public void RepeatString_OnEmptyOrNull(string stringValue, string expected, int numberOfTimesToRepeat)
+        {
+            // Act
+            var exception = Assert.Throws<Exception>(() => SimpleTasks.RepeatString(stringValue, numberOfTimesToRepeat));
+
+            // Assert
+            Assert.Equal(expected, exception.Message);
+        }
+
+        [Theory]
+        [MemberData(nameof(SimpleTasksTestMemberData.ReturnOddNumbersBetween1And100Data), MemberType = typeof(SimpleTasksTestMemberData))]
+        public void ReturnOddNumbersBetween1And100(List<int> expected)
+        {
+            // Act
+            var result = SimpleTasks.ReturnOddNumbersBetween1And100();
+
+            // Assert
+            for (var i = 0; i < result.Count; i++)
+            {
+                Assert.Equal(expected[i], result[i]);
+                _outputHelper.WriteLine($"One of the odd number are: {result[i]}.");
+            }
         }
     }
 }
